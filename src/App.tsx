4510c3f5b87ac6e7dd6d0fb0f80d45db1a61e2f6
Rayton Lin <linsihan9@gmail.com>
2022-07-24 17:54:24 -0400

    Add owner contact, single listing

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,8 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import { db } from './firebase.config'
 import PrivateRoute from './components/PrivateRoute'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   const database = db;
@@ -22,6 +24,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing />} />
           <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
@@ -30,6 +33,7 @@ function App() {
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/contact/:ownerId' element={<Contact />} />
           <Route
             path="*"
             element={<Navigate to="/" />}
---
+++ src/pages/Listing.tsx
@@ -0,0 +1,96 @@
+import { getAuth } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase.config";
+import shareIcon from '../assets/svg/shareIcon.svg'
+
+function Listing() {
+  const [listing,setListing] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [shareLinkCopied, setShareLinkCopied] = useState<boolean>(false)
+
+  const navigate = useNavigate()
+  const params = useParams()
+  const auth = getAuth()
+
+  useEffect(() => {
+    const fetchListing = async (): Promise<void> => {
+      const docRef = doc(db, 'listings', params.listingId!)
+      const docSnap = await getDoc(docRef)
+
+      if (docSnap.exists()) {
+        console.log(docSnap.data())
+        setListing(docSnap.data())
+        setLoading(false)
+      }
+    }
+
+    fetchListing()
+  }, [navigate, params.listingId])
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    <main>
+      {/* Slider */}
+      
+      <div className="shareIconDiv" onClick={() => {
+        navigator.clipboard.writeText(window.location.href)
+        setShareLinkCopied(true)
+        setTimeout(() => {
+          setShareLinkCopied(false)
+        }, 2000)
+      }}>
+        <img src={shareIcon} alt="" />
+      </div>
+
+      {shareLinkCopied && 
+        <p className="linkCopied">
+          Link copied to clipboard.
+        </p>
+      }
+
+      <div className="listingDetails">
+        <p className="listingName">
+          {listing.name} - ${listing.offer ? listing.discountedPrice : listing.regularPrice}
+        </p>
+        <p className="listingLocation">
+          {listing.location}
+        </p>
+        <p className="listingType">
+          {listing.type === 'rent' ? 'Rent' : 'Sale'}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            { listing.bedrooms !== 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
+          </li>
+          <li>
+            { listing.bathrooms !== 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}
+          </li>
+          <li> { listing.parking && 'Parking Spot' } </li>
+          <li> { listing.furnished && 'Furnished Place' } </li>
+
+          {/* Map */}
+
+          { auth.currentUser?.uid !== listing.userRef && (
+            <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className='primaryButton'>
+              Contact item owner
+            </Link>
+          )}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Listing;
