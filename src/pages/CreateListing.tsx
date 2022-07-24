8a6743cf194612b87072530391d7d772ed574732
Rayton Lin <linsihan9@gmail.com>
2022-07-24 00:16:12 -0400

    Add geocode and image store, must fix bug on storageRef

--- src/pages/CreateListing.tsx
+++ src/pages/CreateListing.tsx
@@ -3,6 +3,9 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from "../components/Spinner"
 import { toast } from "react-toastify"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase.config'
+import { v4 as uuidv4 } from 'uuid'
 
 
 function CreateListing() {
@@ -51,7 +54,7 @@ function CreateListing() {
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoading(true)
-    if (discountedPrice >= regularPrice) {
+    if (offer && discountedPrice >= regularPrice) {
       toast.error('Discounted Price must be less than Regular Price')
       setLoading(false)
       return
@@ -71,9 +74,7 @@ function CreateListing() {
 
     if (geoEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?
-        address=${address}&
-        key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       )
       const data = await response.json()
 
@@ -91,6 +92,55 @@ function CreateListing() {
       geolocation.lng = longitude
       location = address
     }
+
+    const storeImage = async (image: any) => {
+      return new Promise((resolve, reject) => {
+        
+        const storage = getStorage()
+        const fileName = `${auth.currentUser!.uid}-${image.name}-${uuidv4()}`
+        
+        const storageRef = ref(storage, 'images/' + fileName)
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on('state_changed',
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused');
+              break;
+            case 'running':
+              console.log('Upload is running');
+              break;
+          }
+        }, 
+        (error) => {
+          reject(error)
+        }, 
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL)
+          });
+        }
+        );
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+      ).catch(() => {
+        setLoading(false)
+        toast.error('Images unable to be uploaded')
+        return
+      }
+    )
+    
+
+    console.log(imgUrls)
 
     setLoading(false)
   }
@@ -281,16 +331,17 @@ function CreateListing() {
                 No
               </button>
             </div>
+            <div>
+              <label className="formLabel">Address</label>
+              <textarea 
+                className="formInputAddress"
+                id='address'
+                value={address}
+                onChange={onMutate}
+                required
+              />
+            </div>
           </div>
-
-          <label className="formLabel">Address</label>
-          <textarea 
-            className="formInputAddress"
-            id='address'
-            value={address}
-            onChange={onMutate}
-            required
-          />
 
           {!geoEnabled &&
             <div>
@@ -319,7 +370,8 @@ function CreateListing() {
             </div>
           }
 
-          <div className="formPriceDiv">
+          <div>
+            <label className='formLabel'>Price</label>
             <input
               className='formInputSmall'
               type='number'
